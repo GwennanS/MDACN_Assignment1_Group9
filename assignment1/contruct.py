c210f104863ec9e75d2daa0bee027724649c6fe9
Stefan Petrescu <stefan.petrescu997@gmail.com>
2021-03-03 19:59:44 +0200

    Add code for q14

--- assignment1/contruct.py
+++ assignment1/contruct.py
@@ -15,8 +15,8 @@ class FlatSet(object):
         self.pp = pathpy.TemporalNetwork()
         self.ff = df.values.tolist()
         for item in self.ff:
-            self.pp.add_edge(item[0], item[1], item[2], timestamp_format='%S')
-        self.nn = pathpy.Network.from_temporal_network(self.pp)
+            self.pp.add_edge(item[0], item[1], item[2], directed=False, timestamp_format='%S')
+        self.nn = pathpy.Network.from_temporal_network(self.pp, directed=False)
         print("set all package networks")
 
         self.N = self.nn.ncount()
@@ -32,10 +32,6 @@ class FlatSet(object):
         self.average_clustering = nx.average_clustering(self.xx)
         self.diameter = nx.diameter(self.xx)
 
-        # self.strenght = [
-        #     (i + 1, list(self.nn.nodes.values())[i].get('outweight') + list(self.nn.nodes.values())[i].get('inweight'))
-        #     for i in range(0, len(self.nn.nodes))]
-        # self.degrees = [(i + 1, self.nn.degrees()[i]) for i in range(0, len(self.nn.nodes))]
         print("set everything")
 
     def infected_end(self, start):
@@ -92,25 +88,92 @@ if __name__ == '__main__':
     df3 = pd.DataFrame(G3List, columns=['node1', 'node2', 'timestamp'])
     G3 = FlatSet(df3)
 
-    print(G1.pp)
-    print(G2.pp)
-    print(G3.pp)
+    #compute probability density for G1
+    ####################################################################
+    intervals_g1 = []
+    
+    for edgeval in G1.nn.edges:
+        current_intervals = []
+        for edgeval_current in G1.pp.tedges[::2]:
+            if edgeval[0] == edgeval_current[0] and edgeval[1] == edgeval_current[1]:
+                current_intervals.append(edgeval_current[2])
+        
+        current_intervals.pop(0) # do this to remove the edge itself, e.g. [a, a] is no interval
+        #print(current_intervals)
 
-    # this plot is weight distribution per edge hist
-    ax1 = plt.subplot(311)
-    ax1.hist([int(e['weight']) for e in G1.nn.edges.values()],
-             bins=int(max([e['weight'] for e in G1.nn.edges.values()])))
-    ax1.set_yscale('log')
-    ax1.set_title("weight G1")
-    ax2 = plt.subplot(312)
-    ax2.hist([int(e['weight']) for e in G2.nn.edges.values()],
-             bins=int(max([e['weight'] for e in G2.nn.edges.values()])))
-    ax2.set_yscale('log')
-    ax2.set_title("weight G2")
-    ax3 = plt.subplot(313)
-    ax3.hist([int(e['weight']) for e in G3.nn.edges.values()],
-             bins=int(max([e['weight'] for e in G3.nn.edges.values()])))
-    ax3.set_yscale('log')
-    ax3.set_title("weight G3")
+        for x, y in zip(current_intervals[0::], current_intervals[1::]): 
+            intervals_g1.append(y-x) 
+        #print(intervals)
+
+        print("G1 len(intervals): " + str(len(intervals_g1)))
+    ####################################################################
+
+
+    #compute probability density for G2
+    ####################################################################
+    intervals_g2 = []
+    
+    for edgeval in G2.nn.edges:
+        current_intervals = []
+        for edgeval_current in G2.pp.tedges[::2]:
+            if edgeval[0] == edgeval_current[0] and edgeval[1] == edgeval_current[1]:
+                current_intervals.append(edgeval_current[2])
+        
+        current_intervals.pop(0) # do this to remove the edge itself, e.g. [a, a] is no interval
+        #print(current_intervals)
+
+        for x, y in zip(current_intervals[0::], current_intervals[1::]): 
+            intervals_g2.append(y-x) 
+        #print(intervals)
+
+        print("G2 len(intervals): " + str(len(intervals_g2)))
+    ####################################################################
+
+
+    #compute probability density for G3
+    ####################################################################
+    intervals_g3 = []
+    
+    for edgeval in G3.nn.edges:
+        current_intervals = []
+        for edgeval_current in G3.pp.tedges[::2]:
+            if edgeval[0] == edgeval_current[0] and edgeval[1] == edgeval_current[1]:
+                current_intervals.append(edgeval_current[2])
+        
+        current_intervals.pop(0) # do this to remove the edge itself, e.g. [a, a] is no interval
+        #print(current_intervals)
+
+        for x, y in zip(current_intervals[0::], current_intervals[1::]): 
+            intervals_g3.append(y-x) 
+        #print(intervals)
+
+        print("G3 len(intervals): " + str(len(intervals_g3)))
+    ####################################################################
+
+    number_of_bins_for_hist = 2000 #number of bins in graphs
+
+    #plot probability density for G1
+    f1 = plt.figure(1)
+    n, bins, patches = plt.hist(intervals_g1, bins=number_of_bins_for_hist,density=True)
+    plt.xlabel("Values")
+    plt.ylabel("Frequency")
+    plt.title("G1 - Graph data")
+    f1.show()
+
+    #plot probability density for G2
+    f2 = plt.figure(2)
+    n, bins, patches = plt.hist(intervals_g2, bins=number_of_bins_for_hist,density=True)
+    plt.xlabel("Values")
+    plt.ylabel("Frequency")
+    plt.title("G2 - Graph 2")
+    f2.show()
+
+    #plot probability density for G3
+    f3 = plt.figure(3)
+    n, bins, patches = plt.hist(intervals_g3, bins=number_of_bins_for_hist,density=True)
+    plt.xlabel("Values")
+    plt.ylabel("Frequency")
+    plt.title("G3 - Graph 3")
+    f3.show()
 
     plt.show()
